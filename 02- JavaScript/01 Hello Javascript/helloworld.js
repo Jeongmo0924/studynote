5ef4805566ee459ae29e0c4c19ccff87a511d967
Jeongmo <operbjm@naver.com>
2022-01-24 21:51:49 +0900

    2022-01-24 강의내용 추가

--- 02- JavaScript/01 Hello Javascript/helloworld.js
+++ 02- JavaScript/01 Hello Javascript/helloworld.js
@@ -6,16 +6,16 @@ console.group("MyMessage1");
     console.log('hello world2');
     console.log('hello world3');
     console.log('hello world4');
-    console.log('hello world5');
+    console.log('hello world5 안녕하세요 ddddd');
 console.groupEnd();
 
 console.group("MyMessage2"); // 출력하는 내용을 그룹으로 묶음
     console.log('hello world6');
     console.log('hello world7');
-    console.group("MyMessage2-1");
+    console.group("MyMessage2-1"); // 그룹 안에 하위그룹 생성
         console.log('hello world8');
         console.log('hello world9');
         console.log('hello world10');
-    console.groupEnd();
+    console.groupEnd(); // 하위 그룹으로 묶기
     console.log('hello world11');
 console.groupEnd(); // 그룹으로 묶기 끝!!
---
+++ 02- JavaScript/02 변수와 데이터타입/03. 상수.js
@@ -0,0 +1,8 @@
+"use strict";
+
+/** 상수 */
+const MY_VALUE1 = 123;
+console.log(MY_VALUE1);
+
+// const로 선언된 변수이므로 값을 변경할 수 없다. (Error = TypeError: Assignment to constant variable.)
+MY_VALUE1 = 234;
