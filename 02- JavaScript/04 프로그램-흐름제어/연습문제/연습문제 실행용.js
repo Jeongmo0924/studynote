0ff5dc068f9e60497487a09da92a41cf53d31cbb
jeongmo0924 <operbjm@naver.com>
2022-01-26 16:55:57 +0900

    노트북 첫 커밋

---
+++ 02- JavaScript/04 프로그램-흐름제어/연습문제/연습문제 실행용.js
@@ -0,0 +1,24 @@
+// const 수학 = "B";
+
+// switch (수학) {
+//     case 'A':
+//     case 'B':
+//     case 'C':
+//         console.log("이 과목을 Pass 했습니다.");
+//         break;
+//     default:
+//         console.log("이 과목을 Pass하지 못했습니다.");
+//         break;
+// }
+
+const 수학 = "B";
+
+if (수학 == "A") {
+    console.log("이 과목을 Pass 했습니다.");
+} else if (수학 == "B") {
+    console.log("이 과목을 Pass 했습니다.");
+} else if (수학 == "C") {
+    console.log("이 과목을 Pass 했습니다.");
+} else {
+    console.log("이 과목을 Pass하지 못했습니다.");
+}
