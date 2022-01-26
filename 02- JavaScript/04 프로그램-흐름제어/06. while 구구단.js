82dc385d411adf7c1edb1be73186abd24d54ba9b
jeongmo0924 <operbjm@naver.com>
2022-01-26 20:57:04 +0900

    쉬는시간 중간 커밋

---
+++ 02- JavaScript/04 프로그램-흐름제어/06. while 구구단.js
@@ -0,0 +1,28 @@
+/** 구구단 13단 출력하기 */
+// y가 1부터 14보다 작은 동안 1씩 증가하면서 반복하므로 반복의 범위는 1~13
+// 반복문이 수행되는 동안 y값에 13을 곱해서 z값을 생성
+
+let y = 1;
+
+while (y <= 13) {
+    const z = 13 * y;
+    console.log("13 * %d = %d", y, z);
+    y++;
+}
+
+let i = 7;
+let a = 0;
+
+while (i <= 28) {
+    if (6 < i && i <= 11) {
+        let a = i
+        console.log("c조의 2월 대면 날짜는 2월 %d일 입니다.", a);
+    } else if (11 < i && i <= 18) {
+        let a = i + 2;
+        console.log("c조의 2월 대면 날짜는 2월 %d일 입니다.", a);
+    } else if (18 < i && i <= 25) {
+        let a = i + 4;
+        console.log("c조의 2월 대면 날짜는 2월 %d일 입니다.", a);
+    } else {}
+    i+=3;
+}
--- 02- JavaScript/04 프로그램-흐름제어/연습문제/연습문제 실행용.js
+++ 02- JavaScript/04 프로그램-흐름제어/연습문제/연습문제 실행용.js
@@ -1,24 +1,57 @@
+// // const 수학 = "B";
+
+// // switch (수학) {
+// //     case 'A':
+// //     case 'B':
+// //     case 'C':
+// //         console.log("이 과목을 Pass 했습니다.");
+// //         break;
+// //     default:
+// //         console.log("이 과목을 Pass하지 못했습니다.");
+// //         break;
+// // }
+
 // const 수학 = "B";
 
-// switch (수학) {
-//     case 'A':
-//     case 'B':
-//     case 'C':
-//         console.log("이 과목을 Pass 했습니다.");
-//         break;
-//     default:
-//         console.log("이 과목을 Pass하지 못했습니다.");
-//         break;
+// if (수학 == "A") {
+//     console.log("이 과목을 Pass 했습니다.");
+// } else if (수학 == "B") {
+//     console.log("이 과목을 Pass 했습니다.");
+// } else if (수학 == "C") {
+//     console.log("이 과목을 Pass 했습니다.");
+// } else {
+//     console.log("이 과목을 Pass하지 못했습니다.");
 // }
 
-const 수학 = "B";
+// let x = 2;
+// let i = 1;
 
-if (수학 == "A") {
-    console.log("이 과목을 Pass 했습니다.");
-} else if (수학 == "B") {
-    console.log("이 과목을 Pass 했습니다.");
-} else if (수학 == "C") {
-    console.log("이 과목을 Pass 했습니다.");
-} else {
-    console.log("이 과목을 Pass하지 못했습니다.");
+// while (i <= 10){
+//     console.log("이진수 %d개는 %d개의 정보를 표시가능", i, x);
+//     x = x * 2;
+//     i++;
+// }
+
+
+// const 수학 = "B";
+
+// if (수학 == "A" || 수학 == "B" || 수학 == "C") {
+//     console.log("이 과목을 Pass 했습니다.");
+// } else {
+//     console.log("이 과목을 Pass하지 못했습니다.");
+// }
+
+
+
+// let x = 2;
+// let i = 1;
+
+// while (i <= 10){
+//     console.log("이진수 %d개는 %d개의 정보를 표시가능", i, x);
+//     x = x * 2;
+//     i++;
+// 
+
+for (let x = 2, i = 1; i <= 10; i++, x = x * 2) {
+    console.log("이진수 %d개는 %d개의 정보를 표시가능", i, x);
 }
