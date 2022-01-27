e93c109f3240084d4244f09099e19ba21cb67c98
jeongmo0924 <operbjm@naver.com>
2022-01-27 15:00:43 +0900

    연습.js

---
+++ 02- JavaScript/05 기본문법 활용하기/연습.js
@@ -0,0 +1,16 @@
+// const math2 = "D";
+
+// if (math2 === "A" || math2 ===  "B" || math2 ===  "C") {
+//     console.log("이 과목을 Pass 했습니다.");
+// } else {
+//     console.log("이 과목을 Pass하지 못했습니다.");
+// }
+
+
+const math2 = "D";
+
+if (math2 ===("A" || "B" || "C")) {
+    console.log("이 과목을 Pass 했습니다.");
+} else {
+    console.log("이 과목을 Pass하지 못했습니다.");
+}
