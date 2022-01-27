5c2e700cf47e18e0d08e6ec2d490b0b5144c4e6e
jeongmo0924 <operbjm@naver.com>
2022-01-27 15:51:04 +0900

    강의 시작 전 동기화

--- 02- JavaScript/05 기본문법 활용하기/01. 변수의 스코프.js
+++ 02- JavaScript/05 기본문법 활용하기/01. 변수의 스코프.js
@@ -8,8 +8,8 @@
 /** 1) var 중복 선언 */
 // 조건문이 실행되지 않는 경우
 if (false) {
-    var num1 = 100;
-    console.log("블록 안:" + num1);
+  var num1 = 100;
+  console.log("블록 안:" + num1);
 }
 
 // if 문의 실행 여부에 따라 num1이 선언되므로
@@ -19,19 +19,19 @@ console.log("블록 밖:" + num1);
 
 // 조건문이 실행되는 경우
 if (true) {
-    var num2 = 100;
-    console.log("블록 안:" + num2);
+  var num2 = 100;
+  console.log("블록 안:" + num2);
 }
 
 console.log("블록 밖:" + num2);
 
 /** 2) let 중복 선언 */
-let num3 = 100
+let num3 = 100;
 
 if (true) {
-    // 블록 밖에서 생성된 변수를 블록 안에서 사용 가능
-    let num4 = num3 + 100;
-    console.log("블록 안:" + num4);
+  // 블록 밖에서 생성된 변수를 블록 안에서 사용 가능
+  let num4 = num3 + 100;
+  console.log("블록 안:" + num4);
 }
 
 // let으로 선언된 변수는 if문의 실행 여부와 상관 없이 블록을 빠져나올 수 없다.
@@ -40,13 +40,13 @@ console.log("블록 밖:" + num4);
 
 /** 3) for문의 초기식을 var로 선언한 경우 */
 for (var i = 0; i < 10; i++) {
-    console.log("반복문 안:::" + i);
+  console.log("반복문 안:::" + i);
 }
 console.log("반복문 밖:::" + i);
 
 /** 4) for문의 초기식을 let으로 선언한 경우 */
 for (let j = 0; j < 10; j++) {
-    console.log("반복문 안:::" + j);
+  console.log("반복문 안:::" + j);
 }
 
 // for문의 초기식도 {}에 속한 것으로 보기 때문에 j값은 for 블록을 빠져나올 수 없다.
@@ -55,7 +55,7 @@ console.log("반복문 밖:::" + j);
 /** 5) 선언되지 않은 변수의 경우 */
 // let 키워드는 반드시 선언 -> 할당의 순서로만 사용 가능함.
 x = 100; // 할당
-let x;   // 선언
+let x; // 선언
 console.log(x);
 
 // var 키워드는 할당 후 선언이 가능함.
